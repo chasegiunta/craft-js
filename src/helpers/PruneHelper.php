a6bc1cd65a6eda2d41305feb2d25787a22ec8721
Chase Giunta <me@chasegiunta.com>
2024-03-01 08:04:41 -0600

    cleanup

--- src/helpers/PruneHelper.php
+++ src/helpers/PruneHelper.php
@@ -24,7 +24,6 @@ class PruneHelper
       $data = [$data];
     }
 
-    $expressionLanguage = new ExpressionLanguage();
     $pruneDefinition = json_decode($pruneDefinition, true);
 
     if (!is_array($pruneDefinition) || !count($pruneDefinition) > 0) {
@@ -77,7 +76,7 @@ class PruneHelper
 
           // Loop through [nested, property, keys]
           foreach ($nestedPropertyKeys as $nestedPropertyKey) {
-            if (($isElement && $object->hasProperty($fieldDefinition)) || $object && property_exists($object, $fieldDefinition)) {
+            if (($isElement && $object->canGetProperty($fieldDefinition)) || $object && property_exists($object, $fieldDefinition)) {
               $field = $object->$fieldDefinition;
 
               // $prunedData[$elementIndex][$fieldDefinition] = [];
@@ -95,7 +94,7 @@ class PruneHelper
           ];
         }
 
-        if (($isElement && $object->hasProperty($fieldDefinition)) || $object && property_exists($object, $fieldDefinition)) {
+        if (($isElement && $object->canGetProperty($fieldDefinition)) || $object && property_exists($object, $fieldDefinition)) {
           $field = $object->$fieldDefinition;
           $fieldValueType = gettype($field);
 
@@ -145,90 +144,6 @@ class PruneHelper
     }
 
     return $prunedData;
-
-    // Loop through prune array and note items that contain parentheses
-    // These items will need to be handled differently
-    // $parenthesesItems = [];
-    // foreach ($prune as $key => $value) {
-    //   if (strpos($value, '(') !== false) {
-    //     $parenthesesItems[] = $value;
-    //   }
-    // }
-    // Remove parentheses items from prune array
-    // $prune = array_diff($prune, $parenthesesItems);
-
-    // $nestedProperties = [];
-
-    // // Given that each item in $parentheses is a string that has an $element property followed by a method call
-    // // We need to loop through each item and get the nested property on the $element property
-    // foreach ($parenthesesItems as $item) {
-    //   $item = explode('(', $item);
-    //   $elementProperty = trim($item[0]);
-    //   $methods = trim($item[1], ')');
-    //   $methods = explode('.', $methods);
-    //   $methods = explode(',', $methods[0]);
-
-    //   //remove whitespace in $methods
-    //   $methods = array_map('trim', $methods);
-
-    //   foreach ($data as $key => $el) {
-
-    //     // TODO: Determine if we're accessing SuperTable, Matrix, or some other element
-    //     // Assume we're accessing SuperTable
-    //     $elements = $el[$elementProperty]->all();
-
-    //     foreach ($elements as $element) {
-    //       if (!is_object($element)) {
-    //         continue;
-    //       }
-    //       $craftNamespace = 'craft\\elements\\'; // Specify the namespace you want to check
-
-    //       $className = get_class($element);
-    //       if (strpos($className, $craftNamespace) === 0) {
-    //         // This is a Craft element
-    //         foreach ($methods as $method) {
-    //           $value = $element->$method;
-    //           // add to $nestedProperties
-    //           $nestedProperties[$key][$elementProperty][$method] = $value;
-    //         }
-    //       } else if ($element instanceof \verbb\supertable\elements\SuperTableBlockElement) {
-    //         // If $element is an instance of SuperTableBlockElement
-    //         // Loop through each field in the SuperTable field layout
-    //         $values = [];
-    //         foreach ($element->getFieldLayout()->getCustomFields() as $field) {
-    //           // if $field->handle is in $methods, get the value
-    //           if (in_array($field->handle, $methods)) {
-    //             $value = $element->getFieldValue($field->handle);
-    //             // add to $nestedProperties
-    //             $nestedProperties[$key][$elementProperty][$field->handle] = $value;
-
-    //             // $data[$key][$entryProperty][$field->handle] = $value;
-    //           }
-
-    //           //     // $data[$key][$entryProperty][$method] = $value;
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
-
-    // if (is_array($data) && count($data) > 0) {
-    //   // .all() returns an array of objects
-    //   // Loop through data and prune each entry
-    //   foreach ($data as $key => $entry) {
-    //     $entry = $entry->toArray();
-    //     $data[$key] = array_intersect_key($entry, array_flip($prune));
-    //   }
-    // } else {
-    //   // .one() returns a single object
-    //   $data = array_intersect_key($data->toArray(), array_flip($prune));
-    // }
-
-    // // Merge $nestedProperties to $data
-    // foreach ($nestedProperties as $key => $value) {
-    //   $data[$key] = array_merge($data[$key], $value);
-    // }
-
   }
 
   private function getRelatedElementData($field, $isNested = false)
